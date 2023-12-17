8cb5e43043d8d6acd54dec7acca4b8b12a82785b
jvcostales <101704346+jvcostales@users.noreply.github.com>
2023-12-17 16:51:53 +0800

    Add files via upload

---
+++ cloudcomputingscript.js
@@ -0,0 +1,24 @@
+function highlightText(element, searchText) {
+  var content = element.textContent || element.innerText;
+  var caseInsensitiveText = searchText.toLowerCase();
+
+  if (content.toLowerCase().includes(caseInsensitiveText)) {
+    var regex = new RegExp('(' + escapeRegExp(caseInsensitiveText) + ')', 'gi');
+    content = content.replace(regex, '<span class="highlight">$1</span>');
+    element.innerHTML = content;
+  }
+}
+
+function findWord() {
+  var searchText = document.getElementById('searchInput').value;
+  var texts = document.querySelectorAll('.searchword');
+
+  texts.forEach(function (element) {
+    element.innerHTML = element.textContent || element.innerText;
+    highlightText(element, searchText);
+  });
+}
+
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
